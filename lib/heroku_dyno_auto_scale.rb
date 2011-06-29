cd27afc7e536c3bd7a78aed3ed110f9252b3f5ae
Jonathan Owens <jonathan@onthecity.org>
2011-06-29 10:58:37 -0700

    memoize dyno count during scale_dynos run, different default scale config scheme

--- lib/heroku_dyno_auto_scale.rb
+++ lib/heroku_dyno_auto_scale.rb
@@ -13,40 +13,46 @@ module HerokuDynoAutoScale
       def dynos=(qty)
         @@heroku.set_dynos(ENV['HEROKU_APP'], qty)
       end
+      
+      def default_scaling
+        [
+          { :rpm => 100,  :dynos => 10 },
+          { :rpm => 200,  :dynos => 15 },
+          { :rpm => 500,  :dynos => 20 },
+          { :rpm => 1000, :dynos => 30 },
+        ]
+      end
 
       def scaling_configuration=(scale_configuration)
-        default_scaling = 
-          [
-            { :rpm => 100,  :dynos => 10 },
-            { :rpm => 200,  :dynos => 15 },
-            { :rpm => 500,  :dynos => 20 },
-            { :rpm => 1000, :dynos => 30 },
-          ]
         @@scale_configuration = scale_configuration || default_scaling
       end
       
       def scaling_configuration
-        @@scale_configuration
+        @@scale_configuration || default_scaling
       end
       
       def scale_dynos(rpm)
         self.scaling_configuration.reverse_each do |scale_info|
           # Run backwards so it gets set to the highest value first
-
+          dynos = self.dynos
           # If we have an rpm  greater than or equal to the dyno limit for our configuration
           if rpm >= scale_info[:rpm]
             # Set the number of workers unless they are already set to a level we want. 
-            if self.dynos <= scale_info[:dynos]
+            if dynos <= scale_info[:dynos]
               self.dynos = scale_info[:dynos]
               return scale_info[:dynos]
+            else
+              return dynos
             end
           # Otherwise we have a rpm lower than the upscale threshold 
           elsif rpm < scale_info[:rpm]
-            if self.dynos > scale_info[:dynos]
+            if dynos > scale_info[:dynos]
               self.dynos = scale_info[:dynos]
               # Return here so we don't keep looping.
               # This will force dynos to only scale one increment every run.
               return scale_info[:dynos]
+            else
+              return dynos
             end
           end
         end
