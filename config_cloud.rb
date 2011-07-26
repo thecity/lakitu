7d00a92892e209cf936c461ef3e4e43b99af656f
Jonathan Owens <jonathan@onthecity.org>
2011-07-26 11:39:27 -0700

    Too many parens

--- config_cloud.rb
+++ config_cloud.rb
@@ -18,7 +18,6 @@ EC2      = Fog::Compute.new(
 RDS      = Fog::AWS::RDS.new( 
             :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
             :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
-           )
             
 HEROKU   = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASS'])
 
