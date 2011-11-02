1d0585d9b6416272ce20c25f71ce044acd8e4eb3
Jonathan Owens <jonathan@onthecity.org>
2011-11-02 11:09:48 -0700

    Add a redis server check for memory usage and awakeness

--- cloud_rider_task.rb
+++ cloud_rider_task.rb
@@ -5,9 +5,11 @@ puts "Starting Lakitu"
 required_vars = %w(HEROKU_USER HEROKU_PASS HEROKU_APP 
                    AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY 
                    RDS_DATABASE_ID
-                   MEMCACHED_NAME_PREFIX REDIS_URL
+                   MEMCACHED_NAME_PREFIX 
+                   RESQUE_REDIS_URL RESQUE_REDIS_NAMESPACE
                    SENDGRID_DOMAIN SENDGRID_USERNAME SENDGRID_PASSWORD
-                   ALERTS_EMAIL)
+                   ALERTS_EMAIL
+                   REDIS_SERVERS REDIS_MEMORY_THRESHOLD)
                    # If dyno scaling was on you'd need these too.
                    # NEW_RELIC_API_KEY NEW_RELIC_ID NEW_RELIC_APPID 
                    
@@ -18,9 +20,10 @@ end
 # Synced logging for puts
 STDERR.sync = STDOUT.sync = true
 
-every(2.minute,  'worker_scaling')             { WORKER_SCALING_QUEUE.push({}) }
+every(2.minute,  'worker_scaling')               { WORKER_SCALING_QUEUE.push({}) }
 # This didn't work right.
 # every(5.minute,  'dyno_scaling')               { DYNO_SCALING_QUEUE.push({})   }
-every(10.minute, 'ec2_checking')               { EC2_CHECKING_QUEUE.push({})   }
-every(15.minute, 'resque_checking')            { RESQUE_CHECKING_QUEUE.push({})}
+every(10.minute, 'ec2_checking')                 { EC2_CHECKING_QUEUE.push({})   }
+every(15.minute, 'resque_checking')              { RESQUE_CHECKING_QUEUE.push({})}
+every(15.minute, 'redis_checking')               { REDIS_CHECKING_QUEUE.push({}) }
 every(1.day,     'rds_snapshot', :at => '11:00') { RDS_SNAPSHOT_QUEUE.push({}) } #UTC suckas
