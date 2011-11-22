2a50df9cde3037e4940d8c3fdbb462321320863e
Jonathan Owens <jonathan@onthecity.org>
2011-11-22 11:21:43 -0800

    Allow customization of config command, added docs around new ENV vars

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ He can:
 * Make sure a set of Heroku config settings stay up to date.
 * Scale resque workers based on queue size.
 * Make sure a set of memcached servers are up.
+* Take an RDS snapshot every so often.
   
 He will:
 
@@ -30,7 +31,19 @@ The prefix of the EC2 Name tags of your memcached servers. Your production Herok
 
 The URL of your Redis server that backs your Resque workers.
 
-* REDIS_URL
+* RESQUE_REDIS_URL
+
+The comma-separated URLs of any other Redis servers you wish to monitor. Optionally, custom configuration commands for each server. 
+Example:
+    REDIS_SERVER_URLS: "redis://username:password@pike.redistogo.com:5044,redis://ip-10-114-119-41.ec2.internal"
+    REDIS_CONFIG_CMDS: "a2352352365f,config"
+
+* REDIS_SERVER_URLS
+* REDIS_CONFIFG_CMDS
+
+The database ID of the RDS server you'd like to snapshot. 10 rolling snapshots will be retained.
+
+* RDS_DATABASE_ID
 
 Sendgrid credentials to deliver alerts.
 
--- lib/queues/redis_checking_queue.rb
+++ lib/queues/redis_checking_queue.rb
@@ -1,26 +1,38 @@
 REDIS_CHECKING_QUEUE = GirlFriday::WorkQueue.new(:redis_checker, :size => 1) do |msg|
   puts "Checking redis"
-  check_servers = ENV['REDIS_SERVERS'].split(',')
-  check_servers.each do |redis_server_url|
-    begin
-      server = Redis.connect(:url => redis_server_url)
-      max_memory  = server.config(:get, 'maxmemory')['maxmemory'].to_f
-      used_memory = server.info['used_memory'].to_f
+  
+  check_servers = ENV['REDIS_SERVER_URLS'].split(',')
+  cfg_cmds      = ENV['REDIS_CONFIG_CMDS'] ? ENV['REDIS_CONFIG_CMDS'].split(',') : [].tap {|cfg| check_servers.count.times { cfg << 'config' }}
+  
+  if check_servers.count != cfg_cmds.count
+    AlertMailer.deliver_alert(
+      'Redis checking misconfigured',
+      "Redis check configuration is incorrect - #{check_servers.count} servers specified but #{cfg_cmds.count} configuration commands were given."
+    )
+  else
+    check_servers.each_with_index do |redis_server_url, i|
+      begin
+        server = Redis.connect(:url => redis_server_url)
+        # redistogo renames config to some obscure sha1, so we have to send it via method_missing
+        max_memory  = server.send(ENV['REDIS_CONFIG_CMDS'][i], :get, 'maxmemory')['maxmemory'].to_f
+        used_memory = server.info['used_memory'].to_f
       
-      used_pct = max_memory.zero? ? 0 : ((used_memory / max_memory)*100).ceil
+        used_pct = max_memory.zero? ? 0 : ((used_memory / max_memory)*100).ceil
 
-      if used_pct > REDIS_MEMORY_THRESHOLD
+        if used_pct > ENV['REDIS_MEMORY_THRESHOLD'].to_i
+          AlertMailer.deliver_alert(
+            'Redis memory threshold exceeded',
+            "Redis server #{redis_server_url} has exceeded #{ENV['REDIS_MEMORY_THRESHOLD']}% of memory usage, with #{used_pct}% used."
+          )
+        end
+      
+      rescue Exception => e
         AlertMailer.deliver_alert(
-          'Redis memory threshold exceeded',
-          "Redis server #{redis_server_url} has exceeded #{REDIS_MEMORY_THRESHOLD}% of memory usage, with #{used_pct}% used."
+          'Exception encountered checking redis',
+          "Exception encountered when checking #{redis_server_url}: #{e}\nBacktrace:\n#{e.backtrace}"
         )
       end
-      
-    rescue Exception => e
-      AlertMailer.deliver_alert(
-        'Exception encountered checking redis',
-        "Exception encountered when checking #{redis_server_url}: #{e}\nBacktrace:\n#{e.backtrace}"
-      )
     end
+    
   end
 end
