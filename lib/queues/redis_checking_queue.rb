c73f26104c804142687c47b3e00a92206d643e58
Jonathan Owens <jonathan@onthecity.org>
2011-11-22 13:57:37 -0800

    Reformat puts on redis checking

--- lib/queues/redis_checking_queue.rb
+++ lib/queues/redis_checking_queue.rb
@@ -19,7 +19,7 @@ REDIS_CHECKING_QUEUE = GirlFriday::WorkQueue.new(:redis_checker, :size => 1) do 
         used_memory = server.info['used_memory'].to_f
       
         used_pct = max_memory.zero? ? 0 : ((used_memory / max_memory)*100).ceil
-        puts "#{redis_server_url} used #{used_pct}% memory used"
+        puts "#{redis_server_url} - #{used_pct}% memory used"
 
         if used_pct > ENV['REDIS_MEMORY_THRESHOLD'].to_i
           AlertMailer.deliver_alert(
